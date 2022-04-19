f4c82878f0f48689fe37a13dc2c848b8fa19805b
Sewek <seweryq@gmail.com>
2022-04-19 20:04:27 +0200

    fixed carousel bug with liblary slickjs

--- js/main.js
+++ js/main.js
@@ -3,9 +3,6 @@
 //variables
 const hamburger = document.querySelectorAll(".hamburger-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const carouselArticles = document.querySelectorAll(".carousel-section__car");
-const carouselPrevBtn = document.querySelector(".prev-btn");
-const carouselNextBtn = document.querySelector(".next-btn");
 const nextSlideButtons = document.querySelectorAll(".next-slide");
 const prevSlideButtons = document.querySelectorAll(".prev-slide");
 const selectedItem = document.getElementById("selected");
@@ -21,30 +18,6 @@ const desktopViewport = window.matchMedia("screen and (min-width:990px)");
 //Event Listners
 hamburger.forEach((item) => {
   item.addEventListener("click", showMenu);
-});
-
-carouselPrevBtn.addEventListener("click", () => {
-  counter--;
-  if (counter === 0) {
-    counter = 1;
-    return;
-  }
-
-  carousel("+");
-  if (window.innerWidth < 990) showOrHideInformation(false);
-});
-
-carouselNextBtn.addEventListener("click", () => {
-  counter++;
-  if (counter === 7) {
-    counter = 6;
-    return;
-  }
-
-  carousel("-");
-  if (window.innerWidth < 990) {
-    showOrHideInformation(false);
-  }
 });
 
 nextSlideButtons.forEach((item) => {
@@ -72,68 +45,6 @@ dropDownItems.forEach((item, index) => {
   }
 });
 
-//setting carousel start values
-carouselArticles.forEach((item) => {
-  if (CarouselLeftStartedValue === 10) {
-    item.style.opacity = `100%`;
-  } else {
-    hideInformation(item);
-  }
-  item.style.left = `${CarouselLeftStartedValue}%`;
-  CarouselLeftStartedValue += 55;
-});
-
-//carousel mechanism
-function carousel(operator) {
-  carouselArticles.forEach((item) => {
-    let leftValue = parseInt(item.style.left);
-
-    if (leftValue === 10) {
-      item.style.opacity = "50%";
-      hideInformation(item);
-    }
-
-    if (operator === "+") {
-      leftValue += 55;
-    } else {
-      leftValue -= 55;
-    }
-
-    item.style.left = `${leftValue}%`;
-
-    if (leftValue === 10) {
-      item.style.opacity = "100%";
-      showCarInformation(item);
-    }
-  });
-}
-
-function hideInformation(item) {
-  if (item.style.left === "10%") return;
-  item.children[2].style.display = "none";
-  item.children[3].style.display = "none";
-  item.children[4].style.display = "none";
-}
-
-function showCarInformation(item) {
-  item.children[2].style.display = "block";
-  item.children[3].style.display = "flex";
-  item.children[4].style.display = "block";
-}
-
-function showOrHideInformation(isDesktop) {
-  console.log(isDesktop);
-  if (isDesktop) {
-    carouselArticles.forEach((item) => {
-      showCarInformation(item);
-    });
-  } else {
-    carouselArticles.forEach((item) => {
-      hideInformation(item);
-    });
-  }
-}
-
 //hamburger menu mechanism
 function showMenu() {
   mobileMenu.classList.toggle("mobile-menu--hidden");
@@ -142,13 +53,6 @@ function showMenu() {
     item.classList.toggle("fa-show");
   });
 }
-
-//Viewport for carousel
-showOrHideInformation(desktopViewport.matches);
-
-desktopViewport.addListener((isDesktop) => {
-  showOrHideInformation(isDesktop.matches);
-});
 
 //slider by slidedjs and jquery
 $(document).ready(function () {
@@ -180,7 +84,6 @@ $(document).ready(function () {
 });
 
 //slider for images section
-
 function nextSlide({ currentTarget }) {
   const ParentElement = currentTarget.parentNode;
   const images = [...ParentElement.querySelectorAll(".img-slider__img")];
@@ -247,3 +150,16 @@ function changeDropDownValue({ currentTarget }) {
 
   dropDown(dropDownContainer);
 }
+
+//carousel mechanism
+
+$(".carousel-section__slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  prevArrow: ".carousel-section__prevBtn",
+  nextArrow: ".carousel-section__nextBtn",
+});
